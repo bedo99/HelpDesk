b43d39c6584ebc28d6282c960d27d814525e744c
Eduardo Bedolla <bedo99.cf@gmail.com>
2020-08-25 11:18:00 -0500

    Implementacion de pagina TicketAdmin

--- DeskProfile/js/sb-admin-2.js
+++ DeskProfile/js/sb-admin-2.js
@@ -59,4 +59,17 @@
     });
   });
 
+  
+
 })(jQuery); // End of use strict
+
+function addRowHandlers() {
+  var rows = document.getElementById("dataTableAdmin").rows;
+  for (i = 0; i < rows.length; i++) {
+      rows[i].onclick = function(){ return function(){
+             var id = this.cells[0].innerHTML;
+             return window.document.location = './TicketDescriptionAdmin.html';
+      };}(rows[i]);
+  }
+}
+window.onload = addRowHandlers();
