d775bfcb61368964321b3876b067a9841c0bd95a
Andrei Montoya <andreiarias53@gmail.com>
2020-10-03 18:54:29 -0500

    Indentacion

--- js/login.js
+++ js/login.js
@@ -3,18 +3,18 @@ const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+    container.classList.add("sign-up-mode");
 });
 
 sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+    container.classList.remove("sign-up-mode");
 });
 
-const formaingresar =  document.getElementById('sign-in-form');
+const formaingresar = document.getElementById('sign-in-form');
 
-formaingresar.addEventListener('submit',(e)=>{
+formaingresar.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
     let correo = formaingresar['correo'].value;
     let contrasena = formaingresar['password'].value;
 
@@ -26,27 +26,27 @@ formaingresar.addEventListener('submit',(e)=>{
             Contrasena: contrasena
         }
     }).then(response => {
-        
+
         let usuario = response.data.res[0][0];
         sessionStorage.setItem("userSesion", JSON.stringify(usuario));
-        switch(usuario.IdTipoUsuario){
-            case 1: 
+        switch (usuario.IdTipoUsuario) {
+            case 1:
                 window.document.location = 'DeskProfile/Especialista.html';
                 break;
-            case 2: 
+            case 2:
                 window.document.location = 'DeskProfile/Admin.html';
                 break;
-            case 3: 
+            case 3:
                 window.document.location = 'DeskProfile/Empresa.html';
                 break;
         }
-        formaingresar.reset();  
-    });      
+        formaingresar.reset();
+    });
 });
 
 const formaregistrate = document.getElementById('sign-up-form');
 
-formaregistrate.addEventListener('submit',(e)=>{
+formaregistrate.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const name = formaregistrate['rnombre'].value;
@@ -69,7 +69,7 @@ formaregistrate.addEventListener('submit',(e)=>{
             'Registro Correcto',
             'Ahora ya puedes iniciar Sesión',
             'success'
-          )
+        )
         formaregistrate.reset();
         container.classList.remove("sign-up-mode");
     });
